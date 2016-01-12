3f552d8f36a6ebd19b3b787b46e15a55400ce817
Rhiojin <coolbway@hotmail.com>
2016-01-12 23:52:38 +0100

    Fixed puddle, stop wallcheck from crashing,

--- Mino/Assets/scripts/game/miscItems/WallsCheck.cs
+++ Mino/Assets/scripts/game/miscItems/WallsCheck.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class WallsCheck : MonoBehaviour {
 	[Tooltip("Use if item needs two oposite walls")] 
-	public bool corridor;
+	public bool corridor;//needs -Vector3.right and Vector3.right 
 	[Tooltip("Use if item needs two close walls = corner")] 
-	public bool corner; 
+	public bool corner; //needs Vector3.forward and Vector3.right
 	[Tooltip("Use if item needs to be against a wall")] 
-	public bool wallMounted;
+	public bool wallMounted; //needs Vector3.right
 
 	// Use this for initialization
 	void Start () {
@@ -19,11 +19,19 @@ public class WallsCheck : MonoBehaviour {
 	// if four times without wall delete
 	// need to check if right is touching a wall, if not delete
 
-		if (RayCast (Vector3.left, 1f)) {
-			Debug.Log("HIT");
-			transform.Rotate(new Vector3(-90,0,0));
-			corridorCheck();
+//		if (RayCast (Vector3.left, 1f)) {
+//			Debug.Log("HIT");
+//			transform.Rotate(new Vector3(-90,0,0));
+//			corridorCheck();
+//		}
+
+		//left + right
+		if (Physics.Raycast (transform.position, transform.right, 0.5f) && Physics.Raycast (-transform.position, transform.right, 0.5f)) {
+			transform.eulerAngles += new Vector3(0,90,0);
+			print ("!");
 		}
+
+
 	}
 
 	bool RayCast(Vector3 dir, float scale){
--- Mino/Assets/scripts/game/miscItems/rotate_floor.cs
+++ Mino/Assets/scripts/game/miscItems/rotate_floor.cs
@@ -7,9 +7,9 @@ public class rotate_floor : MonoBehaviour {
 	// RHEO FIX IT
 	// Use this for initialization
 	void Start () {
-		Quaternion rot = transform.localRotation; 
-		rot.y = Random.Range(-360,360);
-		transform.localRotation = rot;
+		Vector3 rot = transform.localEulerAngles; 
+		rot.y = Random.Range(0,360);
+		transform.localEulerAngles = rot;
 		//transform.localRotation.y = Random.rotation.y;
 		//Destroy (this);
 	}
